a85f22f095a8123e6f066a17f820e9b05007a8fe
Chukwuemeka Mba <76162087+Chukwuemeka-Mba@users.noreply.github.com>
2022-04-13 22:19:50 -0400

    :tada: did some mad shit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
+  const [starWarsData, setStarWarsData] = useState({});
+  const [count, setCount] = useState(0);
+  console.log("Component Rendered");
+  useEffect(
+    function () {
+      console.log("Use Effect Rendered");
+      fetch(`https://swapi.dev/api/people/${count}`)
+        .then((res) => res.json())
+        .then((data) => setStarWarsData(data));
+    },
+    [count]
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+      <h2>The count is {count}</h2>
+      <button
+        type="submit"
+        onClick={() => setCount((prevCount) => prevCount + 1)}
+      >
+        Add Count
+      </button>
     </div>
   );
 }
-
-export default App;
