4e6a6478e7ea11ccda4276b52bc02b80bc7d4d28
Chukwuemeka Mba <76162087+Chukwuemeka-Mba@users.noreply.github.com>
2022-04-13 23:31:45 -0400

    :tada: final commit

--- src/WindowTracker.js
+++ src/WindowTracker.js
@@ -1,5 +1,17 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import "./index.css";
 export default function WindowTracker() {
-  return <h1>Window width: {window.innerWidth}</h1>;
+  const [width, setWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    function watchWidth() {
+      console.log("setting up ...");
+      setWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", watchWidth);
+    return function () {
+      console.log("Cleaning up ...");
+      window.removeEventListener("resize", watchWidth);
+    };
+  }, []);
+  return <h1>Window width: {width}</h1>;
 }
